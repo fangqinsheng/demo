0dd83f434ee3cda5f56e834fb7f0ad0e7cde943a
方帅 <fangshuai@fangshuaideMacBook-Pro.local>
2020-07-23 14:10:34 +0800

    Updates

---
+++ precache-manifest.b362c99d0f8b243c8c00bbc732caa904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31491e7fe581bdba1a86746bab3ac63b",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "8f62455971f89a4ce0a2",
+    "url": "/demo/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "612b4b1c180a0de47f3a",
+    "url": "/demo/static/js/2.6c23e8c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.6c23e8c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f62455971f89a4ce0a2",
+    "url": "/demo/static/js/main.35605ed4.chunk.js"
+  },
+  {
+    "revision": "70116ee023940404078b",
+    "url": "/demo/static/js/runtime-main.fc0458c7.js"
+  }
+]);
